58ad3df4a69d8381fb36b5b2b2b0474817ef60e0
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-10 01:55:13 +0000

    with print of list after to show correct

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 # ******************************
 # Make your Code
 # ******************************
+import random
+
+numbers = []
+
+for i in range(10):
+    numbers.append(random.randint(1, 100))
+
+smallestind = numbers.index(min(numbers))
+
+print(numbers[smallestind])
+print(numbers)
